8c77611311acb83c9a969946b19d8f3c0c18079b
Xia <xxl124730@utdallas.edu>
2019-03-02 20:48:28 -0600

    train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,26 +1,102 @@
+import pandas as pd
+import numpy as np
+import re
+import nltk
 import sys
+from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.externals import joblib
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
 
 
 
 def load_data(database_filepath):
-    
-    
-
+    '''
+    input:
+        database_filepath: The path of sql database
+    output:
+        X: Message as training data
+        Y: Label
+        category_names: Categorical names of labels
+    '''
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('DisasterDataTable',engine)
+    X = df.message.values
+    Y = df.drop(['id','message','original','genre'],axis=1)
+    category_names = list(df.columns[4:])
+    return X,Y,category_names
 
 def tokenize(text):
-    pass
+    '''
+    input:
+        text: Message before tokenization.
+    output:
+        clean_tokens: A list of tokens after tokenization.
+    '''
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    stop_words = stopwords.words("english")
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+    
+    # normalize case and remove punctuation
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    
+    # tokenize text
+    tokens = word_tokenize(text)
+    
+    lemmatizer = WordNetLemmatizer()
 
+    clean_tokens = []
+    for tok in tokens:
+        if tok not in stop_words:
+            clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+            clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 def build_model():
-    pass
-
+    '''
+    input:
+        None
+    output:
+        cv: Model after GridSearch
+    '''
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    parameters = {'clf__estimator__n_estimators': [50, 100, 200],
+              'clf__estimator__min_samples_split': [2, 3, 4],
+              'clf__estimator__min_samples_leaf': [1, 2, 4],
+              'clf__estimator__max_depth': [10, 20, None]
+              }
+    cv = GridSearchCV(pipeline, param_grid=parameters,return_train_score=False)
+    return cv
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = cv.predict(X_test)
+    y_pred = pd.DataFrame(y_pred)
+    #y_test.shape
+    for i in range(y_test.shape[1]):
+        print(accuracy_score(y_test.iloc[:,i], y_pred.iloc[:,i]))
 
 
 def save_model(model, model_filepath):
-    pass
+    joblib.dump(model, model_filepath)
 
 
 def main():
